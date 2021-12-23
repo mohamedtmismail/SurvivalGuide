5ff87f181e23ad8d83a0dd688f396940e7781249
Mohamed Ismail <mi18062@essex.ac.uk>
2021-12-23 05:20:39 +0000

    some final touch

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express')
-const { readFile } = require('fs')
-
 const app = express()
+const { readFile } = require('fs').promises
 
-app.get('/', (request, response) => {
-  readFile('./home.html', 'utf-8', (err, html) => {
-    if (err) {
-      response.status(500).send('sorry, out of order')
-    }
-    response.send(html)
-  })
+app.get('/', async (request, response) => {
+  response.send(await readFile('./home.html', 'utf8'))
 })
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`App available on http://localhost:3000`)
+)
